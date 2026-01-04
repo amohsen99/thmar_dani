0156ad8256094cf8f2400203eadbfd967921f288
mohsen <amohsenmohamed571@gmail.com>
2026-01-04 10:45:16 +0200

    UPD: update attachment in po

--- thamar_purchase_custom/models/purchase_order.py
+++ thamar_purchase_custom/models/purchase_order.py
@@ -8,10 +8,10 @@ class PurchaseOrder(models.Model):
     attachment = fields.Binary(string='Attachment')
     attachment_name = fields.Char(string='Attachment Name')
 
-    def button_confirm(self):
-        # Prevent confirming if no attachment
-        for order in self:
-            if not order.attachment:
-                raise UserError("You must upload an attachment before confirming the order.")
-        # Then call the original confirm logic
-        return super(PurchaseOrder, self).button_confirm()
+    # def button_confirm(self):
+    #     # Prevent confirming if no attachment
+    #     for order in self:
+    #         if not order.attachment:
+    #             raise UserError("You must upload an attachment before confirming the order.")
+    #     # Then call the original confirm logic
+    #     return super(PurchaseOrder, self).button_confirm()
